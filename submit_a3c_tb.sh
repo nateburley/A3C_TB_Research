6512ddd2aef40d24c54205a494736c253bbcc413
Nate Burley <nburley@Nates-MacBook-Pro.local>
2020-08-30 16:50:46 -0700

    Updated submission shell scripts with training time

--- submit_a3c_tb.sh
+++ submit_a3c_tb.sh
@@ -5,14 +5,14 @@
 #PBS -N A3C_TB         				## Job name
 #PBS -M nathaniel.burley@wsu.edu      		## Email of notification, CHANGE TO YOURS
 #PBS -m abe                    			## Email if abort, begins, ends
-#PBS -l walltime=2:00:00                       ## Run time (day:hh:mm:ss), usually 3+ days (currently 2 hrs. for a test)
+#PBS -l walltime=80:00:00                       ## Run time (day:hh:mm:ss), usually 3+ days (currently 2 hrs. for a test)
 #PBS -l mem=32gb                           	## Memory requirements, 16G should be good, 32G to be safe
 #PBS -l nodes=1:ppn=8                    	## Number of processor needed per task, 8 is a good balance between speed vs.#jobs can run
 #PBS -q datascience                             ## queue on "datascience" group, can also use "batch" (I'm not sure what's the diff tho..)
 #PBS -k o					## keep output file
 #PBS -e /data/datascience/A3C_TB_Research/logs/TB       ## error file directory; this is where you can see the output logs
 #PBS -o /data/datascience/A3C_TB_Research/logs/TB       ## output file directory
-#PBS -t 1-6 					## number of trials to run; here submit 3 trials together, usually want 5+ trials to get the average
+#PBS -t 1-3 					## number of trials to run; here submit 3 trials together, usually want 5+ trials to get the average
 
 module load python/3.7
 module load go/1.11.5
