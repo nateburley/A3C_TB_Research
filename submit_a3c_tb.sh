0e1c3adc18b609ec92b7ef769e1a4e48e0ecd49f
Nate Burley <nburley@Nates-MacBook-Pro.local>
2020-09-02 16:57:49 -0700

    Tweaked submission shell scripts

--- submit_a3c_tb.sh
+++ submit_a3c_tb.sh
@@ -5,8 +5,8 @@
 #PBS -N A3C_TB         				## Job name
 #PBS -M nathaniel.burley@wsu.edu      		## Email of notification, CHANGE TO YOURS
 #PBS -m abe                    			## Email if abort, begins, ends
-#PBS -l walltime=72:00:00                       ## Run time (day:hh:mm:ss), usually 3+ days (currently 2 hrs. for a test)
-#PBS -l mem=16gb                           	## Memory requirements, 16G should be good, 32G to be safe
+#PBS -l walltime=3:00:00:00                       ## Run time (day:hh:mm:ss), usually 3+ days (currently 2 hrs. for a test)
+#PBS -l mem=32gb                           	## Memory requirements, 16G should be good, 32G to be safe
 #PBS -l nodes=1:ppn=8                    	## Number of processor needed per task, 8 is a good balance between speed vs.#jobs can run
 #PBS -q datascience                             ## queue on "datascience" group, can also use "batch" (I'm not sure what's the diff tho..)
 #PBS -k o					## keep output file
@@ -14,7 +14,7 @@
 #PBS -o /data/datascience/A3C_TB_Research/logs/TB       ## output file directory
 #PBS -t 1-5 					## number of trials to run; here submit 3 trials together, usually want 5+ trials to get the average
 
-module load python/3.7
+module load python/3.7/gcc/7.3.0
 module load go/1.11.5
 module load singularity/3.4.2/go/1.11.5
 
@@ -32,6 +32,6 @@ singularity exec -H $cwd $image python3 DeepRL/run_experiment.py \
     --parallel-size=16 \
     --max-time-step-fraction=0.5 \
     --use-mnih-2015 --padding=SAME --input-shape=88 \
+    --transformed-bellman \
     --append-experiment-num=${PBS_ARRAYID} \
-    --transformed-bellman \ ## Use the Transformed Bellman operator in the experiment
-    --save-to=/data/datascience/A3C_TB_Research/results/ \
+    --save-to=/data/datascience/A3C_TB_Research/results/TB \
