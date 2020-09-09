6aa2b209c1c4f529923db17e940055432ff8cfbd
Nate Burley <nburley@Nates-MacBook-Pro.local>
2020-09-08 20:54:40 -0700

    Added code to make plots of training results, reward/return histograms

---
+++ DeepRL/analyze_train_results.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+
+# Function that tests my cumsum reward logging, plots histogram (for Yunshu's exercise)
+# IMPORTANT NOTE: The actual training does 5 "trials" or whatever. If it writes all of those to the same file,
+#                 then you need to add some logic that compares the global_t values to see when in the file one
+#                 one trial ends and the next begins
+def explore_row_log(row_log_pkl, TB=True):
+    with open(row_log_pkl, 'rb') as datafile:
+
+        # Plot the histogram(s) for A3C with the Transformed Bellman
+        if (TB == True):
+            # Read in the cumsum rewards
+            raw_returns = []
+            rewards = []
+            tb_returns = []
+            while True:
+                try:
+                    batch = pickle.load(datafile)
+                    for reward, raw_return, batch_return in zip(batch.rewards, batch.batch_raw_rewards, batch.batch_cumsum_rewards):
+                        rewards.append(reward)
+                        raw_returns.append(raw_return)
+                        tb_returns.append(batch_return)
+                except EOFError:
+                    break
+            
+            # Plot a histogram of raw vs. TB returns
+            plt.hist(raw_returns, color="blue", label="Raw Batch Returns", alpha=0.5, bins=15)
+            plt.hist(tb_returns, color="yellow", label="TB Batch Returns", alpha=0.5, bins=15)
+            plt.title("Number of Occurrences of TB Batch Cumulative Rewards vs. Raw Rewards [TB]")
+            plt.xlabel("Value")
+            plt.ylabel("Number of Occurrences")
+            plt.legend()
+            plt.savefig("plots/TB_raw-vs-trans_return_hist.png")
+
+            # Clear the figure
+            plt.clf()
+
+            # Plot a histogram of rewards
+            plt.hist(rewards, color="green", label="Rewards", bins=15)
+            plt.title("Number of Occurrences of Rewards [TB]")
+            plt.xlabel("Value")
+            plt.ylabel("Number of Occurrences")
+            plt.legend()
+            plt.savefig("plots/TB_reward_hist.png")
+        
+        # Plot the histogram(s) for "normal" A3C
+        else:
+            # Read in the cumsum rewards
+            cumsum_rewards = []
+            rewards = []
+            while True:
+                try:
+                    batch = pickle.load(datafile)
+                    for reward, batch_return in zip(batch.rewards, batch.batch_cumsum_rewards):
+                        rewards.append(reward)
+                        cumsum_rewards.append(batch_return)
+                except EOFError:
+                    break
+
+            # Plot historgram of returns
+            plt.hist(cumsum_rewards, color="blue", label="Cum. Sum Rewards", bins=15)
+            plt.title("Number of Occurrences of Cumulative Rewards [A3C]")
+            plt.xlabel("Value")
+            plt.ylabel("Number of Occurrences")
+            plt.legend()
+            plt.savefig("plots/A3C_cumsum_rewards.png")
+
+            # Clear the figure
+            plt.clf()
+
+            # Plot histogram of rewards
+            plt.hist(rewards, color="green", label="Rewards", bins=15)
+            plt.title("Number of Occurrences of Rewards [A3C]")
+            plt.xlabel("Value")
+            plt.ylabel("Number of Occurrences")
+            plt.legend()
+            plt.savefig("plots/A3C_rewards.png")
+        
+
+explore_row_log('results/RowLogs/TB_ROW-LOG.pkl')
+plt.clf() # Clear the figures in-between datasets
+explore_row_log('results/RowLogs/A3C_ROW-LOG.pkl', TB=False)
---
+++ analyze_eval_results.py
@@ -0,0 +1,87 @@
+"""
+This file will plot and analyze the results of training A3C (timesteps and rewards). Later, once
+we have more results, we can examine correlations and relationships in more detail (TODO)
+
+Use: python analyze_training_results.py /path/to/pkl/results plot_name
+Ex.: python analyze_training_results.py /data/datascience/A3C_TB_Research/results/A3Ca3c/MsPacmanNoFrameskip_v4_5/MsPacmanNoFrameskip_v4-a3c-rewards.pkl MsPacman-A3C
+
+Author: Nathaniel M. Burley
+Date: 15th August, 2020
+"""
+# Load modules
+import pickle
+import sys
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Pass in a file with the training rewards
+try:
+    results_file = sys.argv[1]
+    plot_name = sys.argv[2]
+    
+except:
+    print("Don't forget to pass in a file as a command line argument!")
+    result_file = "results_test1/a3c/MsPacmanNoFrameskip_v4_08-07-2020/MsPacmanNoFrameskip_v4-a3c-rewards.pkl"
+    plot_name = "plots/training_results_plot.png"
+
+print("Results file: {}".format(results_file)) # Debugging
+
+
+# Open the file for reading
+with open(results_file, 'rb') as datafile:
+    # Load the file; print the results (for debugging)
+    rewards = pickle.load(datafile)
+
+    # Load file into dataframe (training results)
+    rewards_df = pd.DataFrame.from_dict(rewards['train'], orient='index', columns=['reward', 'trans_reward', 'episode_steps']).reset_index()
+    print("TRAIN REWARDS")
+    print(rewards_df.head(10))
+
+    # Get the rewards and timesteps
+    steps = np.array(rewards_df.index)
+    reward_values = np.array(rewards_df['reward'])
+    
+    # Plot the time steps and the rewards
+    plt.plot(steps, reward_values, color='green', marker='o')
+    plt.xlabel('Number of Timesteps')
+    plt.ylabel('Reward Value')
+    plt.title(plot_name)
+    plt.savefig('plots/' + plot_name + '-train-fig.png')
+    plt.close()
+    
+    ################## Do the same as above, but for the evaluation ################################
+    rewards_eval_df = pd.DataFrame.from_dict(rewards['eval'], orient='index', columns=['reward', 'transformed_reward']).reset_index()
+    print("\nEVALUATION REWARDS")
+    print(rewards_eval_df.head(10))
+
+    # Get the rewards and timesteps
+    eval_steps = np.array(rewards_eval_df.index)
+    reward_eval_values = np.array(rewards_eval_df['reward'])
+    trans_eval_values = np.array(rewards_eval_df['transformed_reward'])
+    
+
+    # Plot the time steps and the rewards
+    plt.plot(eval_steps, reward_eval_values, color='green', marker='o', label='Raw Reward')
+    plt.plot(eval_steps, trans_eval_values, color='red', marker='o', label='Transformed Reward')
+    plt.xlabel('Number of Steps (Thousands)')
+    plt.ylabel('Reward Value')
+    plt.legend(['Raw Reward', 'Transformed Reward'])
+    plt.title(plot_name)
+    plt.savefig('plots/' + plot_name + '-eval-fig.png')
+    plt.close()
+
+"""
+Eval reward is the max score at each
+- training reward is the average for one worker in its 20 steps (or until terminal)
+- updating is done with one worker at a time-- adds how many steps it did and what rewards it got, basically
+
+- transformed reward is computed in the training code, I just need to add more code to log it
+
+- "KL divergence" between the two policies...distance between two distributions? (Could possibly be
+used to compare the policies between two agents)
+    - TB is for learning with high variance. Do NOT need to focus on this right now. 
+
+- Good focus for this week would be a plot of raw reward, and a plot of transformed rewards, so we
+can see if there's a correlation
+"""
